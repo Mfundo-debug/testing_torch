645743fc9a55e10a825dfa19488ba91746ebbd0d
Mfundo-debug <diditmfundo@gmail.com>
2023-09-20 06:46:17 +0200

    updates

--- torchnn1.py
+++ torchnn1.py
@@ -1,5 +1,6 @@
 from turtle import forward, backward
 import torch
+from PIL import Image
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -35,12 +36,22 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = Adam(clf.parameters(), lr=1e-3)
 #Training loop
 if __name__ == '__main__':
-    for epoch in range(10):
-        for batch in datasets:
-            x, y = batch
-            optimizer.zero_grad()
-            y_pred = clf(x)
-            loss = loss_fn(y_pred, y)
-            loss.backward()
-            optimizer.step()
-        print(f"Epoch: {epoch} Loss: {loss.item()}")
+    with open('model.pth', 'rb') as f:
+        clf.load_state_dict(torch.load(f))
+
+    img = Image.open("test.jpeg").convert("L")
+    img_tensor = ToTensor()(img).unsqueeze(0).to("cpu")
+    pred =torch.argmax(clf(img_tensor))
+    print(pred)
+
+    # for epoch in range(10):
+    #     for batch in datasets:
+    #         x, y = batch
+    #         optimizer.zero_grad()
+    #         y_pred = clf(x)
+    #         loss = loss_fn(y_pred, y)
+    #         loss.backward()
+    #         optimizer.step()
+    #     print(f"Epoch: {epoch} Loss: {loss.item()}")
+    # with open("model.pth", "wb") as f:
+    #     torch.save(clf.state_dict(), f)
